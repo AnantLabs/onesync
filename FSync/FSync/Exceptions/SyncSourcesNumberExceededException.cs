3ce33fed4311acaa7398009efc0ee2a1fe899b4e
gclin009 <gclin009@3b441868-20d6-11df-a502-17d33bbe96c3>
2010-03-09 08:08:59 +0000

    Three new exception files added by Thuat.

---
+++ FSync/FSync/Exceptions/SyncSourcesNumberExceededException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneSync.Synchronization
+{
+    public class SyncSourcesNumberExceededException:ApplicationException
+    {
+        public SyncSourcesNumberExceededException(string message) : base(message) { }      
+        public SyncSourcesNumberExceededException() : base() { }
+    }
+}
