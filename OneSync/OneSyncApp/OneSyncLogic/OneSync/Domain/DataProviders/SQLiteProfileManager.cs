7121f12ef1fe0a3063e9931b43e73df985dcb217
deskohet <deskohet@3b441868-20d6-11df-a502-17d33bbe96c3>
2010-03-16 14:31:21 +0000

    Changed DATASOURCE_INFO table name to SYNCCOURCE_INFO.

--- OneSync/OneSyncApp/OneSyncLogic/OneSync/Domain/DataProviders/SQLiteProfileManager.cs
+++ OneSync/OneSyncApp/OneSyncLogic/OneSync/Domain/DataProviders/SQLiteProfileManager.cs
@@ -15,17 +15,6 @@ namespace OneSync.Synchronization
     {
         // TODO: Wrap Update/Add etc with try catch and return false if exception thrown?
 
-        // TODO: Find a better place to define these constant?
-        // Database constants
-        public const string DATABASE_NAME = "data.md";
-        public const string PROFILE_TABLE = "PROFILE";
-        public const string COL_PROFILE_ID = "PROFILE_ID";
-        public const string COL_PROFILE_NAME = "PROFILE_NAME";
-        public const string COL_METADATA_SOURCE_LOCATION = "METADATA_SOURCE_LOCATION";
-        public const string COL_SYNC_SOURCE_ID = "SYNC_SOURCE_ID";
-        public const string COL_SOURCE_ABS_PATH = "SOURCE_ABSOLUTE_PATH";
-        public const string COL_SOURCE_ID = "SOURCE_ID";
-        public const string DATASOURCE_INFO_TABLE = "DATASOURCE_INFO_TABLE";
 
         /// <summary>
         /// Creates an instance of SQLiteProfileManager. Database schema will be created if database file is newly created.
@@ -36,7 +25,7 @@ namespace OneSync.Synchronization
         {
             // Create database schema if necessary
 
-            FileInfo fi = new FileInfo(Path.Combine(this.StoragePath, DATABASE_NAME));
+            FileInfo fi = new FileInfo(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME));
 
             if (!fi.Exists)
             {
@@ -53,15 +42,15 @@ namespace OneSync.Synchronization
         /// </summary>
         public void CreateSchema()
         {
-            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
             {
-                string cmdText = "CREATE TABLE IF NOT EXISTS " + PROFILE_TABLE +
-                                "(" + COL_PROFILE_ID + " VARCHAR(50) PRIMARY KEY, "
-                                + COL_PROFILE_NAME + " VARCHAR(50) UNIQUE NOT NULL, "
-                                + COL_METADATA_SOURCE_LOCATION + " TEXT, "
-                                + COL_SYNC_SOURCE_ID + " VARCHAR (50), "
-                                + "FOREIGN KEY (" + COL_SYNC_SOURCE_ID + ") REFERENCES "
-                                + DATASOURCE_INFO_TABLE + "(" + COL_SOURCE_ID + "))";
+                string cmdText = "CREATE TABLE IF NOT EXISTS " + Configuration.TBL_PROFILE +
+                                "(" + Configuration.COL_PROFILE_ID + " VARCHAR(50) PRIMARY KEY, "
+                                + Configuration.COL_PROFILE_NAME + " VARCHAR(50) UNIQUE NOT NULL, "
+                                + Configuration.COL_METADATA_SOURCE_LOCATION + " TEXT, "
+                                + Configuration.COL_SYNC_SOURCE_ID + " VARCHAR (50), "
+                                + "FOREIGN KEY (" + Configuration.COL_SYNC_SOURCE_ID + ") REFERENCES "
+                                + Configuration.TBL_SYNCSOURCE_INFO + "(" + Configuration.COL_SOURCE_ID + "))";
 
                 db.ExecuteNonQuery(cmdText, false);
             }
@@ -76,21 +65,21 @@ namespace OneSync.Synchronization
 
             try
             {
-                using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+                using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
                 {
-                    string cmdText = "SELECT * FROM " + PROFILE_TABLE +
-                                     " p, " + DATASOURCE_INFO_TABLE +
-                                     " d WHERE p" + "." + COL_SYNC_SOURCE_ID + " = d" + "." + COL_SOURCE_ID;
+                    string cmdText = "SELECT * FROM " + Configuration.TBL_PROFILE +
+                                     " p, " + Configuration.TBL_SYNCSOURCE_INFO +
+                                     " d WHERE p" + "." + Configuration.COL_SYNC_SOURCE_ID + " = d" + "." + Configuration.COL_SOURCE_ID;
 
                     db.ExecuteReader(cmdText, reader =>
                     {
                         while (reader.Read())
                         {
                             // TODO: constructor of Profile takes in more arguments to remove dependency on IntermediaryStorage and SyncSource class.
-                            SyncSource source = new SyncSource((string)reader[COL_SYNC_SOURCE_ID], (string)reader[COL_SOURCE_ABS_PATH]);
-                            IntermediaryStorage mdSource = new IntermediaryStorage((string)reader[COL_METADATA_SOURCE_LOCATION]);
-                            Profile p = new Profile((string)reader[COL_PROFILE_ID],
-                                (string)reader[COL_PROFILE_NAME], source, mdSource);
+                            SyncSource source = new SyncSource((string)reader[Configuration.COL_SYNC_SOURCE_ID], (string)reader[Configuration.COL_SOURCE_ABS_PATH]);
+                            IntermediaryStorage mdSource = new IntermediaryStorage((string)reader[Configuration.COL_METADATA_SOURCE_LOCATION]);
+                            Profile p = new Profile((string)reader[Configuration.COL_PROFILE_ID],
+                                (string)reader[Configuration.COL_PROFILE_NAME], source, mdSource);
                             profiles.Add(p);
                         }
                     }
@@ -110,12 +99,12 @@ namespace OneSync.Synchronization
         {
             Profile p = null;
 
-            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
             {
-                string cmdText = "SELECT * FROM " + PROFILE_TABLE +
-                                 " p, " + DATASOURCE_INFO_TABLE +
-                                 " d WHERE p" + "." + COL_SYNC_SOURCE_ID + " = d" + "." + COL_SOURCE_ID +
-                                 " AND " + COL_PROFILE_ID + " = @pid";
+                string cmdText = "SELECT * FROM " + Configuration.TBL_PROFILE +
+                                 " p, " + Configuration.TBL_SYNCSOURCE_INFO +
+                                 " d WHERE p" + "." + Configuration.COL_SYNC_SOURCE_ID + " = d" + "." + Configuration.COL_SOURCE_ID +
+                                 " AND " + Configuration.COL_PROFILE_ID + " = @pid";
 
 
                 SqliteParameterCollection paramList = new SqliteParameterCollection();
@@ -124,10 +113,10 @@ namespace OneSync.Synchronization
                 db.ExecuteReader(cmdText, paramList, reader => 
                     {
                         // TODO: constructor of Profile takes in more arguments to remove dependency on IntermediaryStorage and SyncSource class.
-                        SyncSource source = new SyncSource((string)reader[COL_SYNC_SOURCE_ID], (string)reader[COL_SOURCE_ABS_PATH]);
-                        IntermediaryStorage mdSource = new IntermediaryStorage((string)reader[COL_METADATA_SOURCE_LOCATION]);
-                        
-                        p = new Profile((string)reader[COL_PROFILE_ID], (string)reader[COL_PROFILE_NAME], source, mdSource);
+                        SyncSource source = new SyncSource((string)reader[Configuration.COL_SYNC_SOURCE_ID], (string)reader[Configuration.COL_SOURCE_ABS_PATH]);
+                        IntermediaryStorage mdSource = new IntermediaryStorage((string)reader[Configuration.COL_METADATA_SOURCE_LOCATION]);
+
+                        p = new Profile((string)reader[Configuration.COL_PROFILE_ID], (string)reader[Configuration.COL_PROFILE_NAME], source, mdSource);
                         
                         return;
                     }
@@ -147,13 +136,13 @@ namespace OneSync.Synchronization
             // Update a profile requires update 2 tables at the same time, 
             // If one update on a table fails, the total update action must fail too.
 
-            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
             {
                 SqliteParameterCollection paramList = new SqliteParameterCollection();
 
-                string cmdText = "UPDATE " + PROFILE_TABLE +
-                                 " SET " + COL_METADATA_SOURCE_LOCATION + " = @mdSource, " +
-                                 COL_PROFILE_NAME + " = @name WHERE " + COL_PROFILE_ID + " = @id;";
+                string cmdText = "UPDATE " + Configuration.TBL_PROFILE +
+                                 " SET " + Configuration.COL_METADATA_SOURCE_LOCATION + " = @mdSource, " +
+                                 Configuration.COL_PROFILE_NAME + " = @name WHERE " + Configuration.COL_PROFILE_ID + " = @id;";
 
                 
                 // Add parameters for 1st Update statement
@@ -162,8 +151,8 @@ namespace OneSync.Synchronization
                 paramList.Add(new SqliteParameter("@id", System.Data.DbType.String) { Value = profile.ID });
 
                 // Concat 2nd Update statement
-                cmdText += "UPDATE " + Configuration.TBL_DATASOURCE_INFO+
-                           " SET " + Configuration.COL_SOURCE_ABSOLUTE_PATH + " = @path WHERE " + COL_SOURCE_ID + " = @gid";
+                cmdText += "UPDATE " + Configuration.TBL_SYNCSOURCE_INFO+
+                           " SET " + Configuration.COL_SOURCE_ABSOLUTE_PATH + " = @path WHERE " + Configuration.COL_SOURCE_ID + " = @gid";
 
                 // Add parameters for 2nd Update statement
                 paramList.Add(new SqliteParameter("@path", System.Data.DbType.String) { Value = profile.SyncSource.Path });
@@ -177,17 +166,17 @@ namespace OneSync.Synchronization
 
         public override bool Delete(Profile profile)
         {
-            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
             {
                 SqliteParameterCollection paramList = new SqliteParameterCollection();
 
-                string cmdText = "DELETE FROM " + Configuration.TBL_DATASOURCE_INFO +
+                string cmdText = "DELETE FROM " + Configuration.TBL_SYNCSOURCE_INFO +
                                  " WHERE " + Configuration.COL_SOURCE_ID + " = @sid;";
                 
                 paramList.Add(new SqliteParameter("@sid", System.Data.DbType.String) { Value = profile.SyncSource.ID });
 
-                cmdText += "DELETE FROM " + PROFILE_TABLE +
-                           " WHERE " + COL_PROFILE_ID + " = @pid";
+                cmdText += "DELETE FROM " + Configuration.TBL_PROFILE +
+                           " WHERE " + Configuration.COL_PROFILE_ID + " = @pid";
 
                 paramList.Add(new SqliteParameter("@pid", System.Data.DbType.String) { Value = profile.ID });
 
@@ -202,11 +191,11 @@ namespace OneSync.Synchronization
             if (this.ProfileExists(profile.Name))
                 throw new ProfileNameExistException("Profile " + profile.Name + " is already created");
 
-            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
             {
-                string cmdText = "INSERT INTO " + PROFILE_TABLE +
-                                 " (" + COL_PROFILE_ID + ", " + COL_PROFILE_NAME +
-                                 " ," + COL_METADATA_SOURCE_LOCATION + ", " + COL_SYNC_SOURCE_ID +
+                string cmdText = "INSERT INTO " + Configuration.TBL_PROFILE +
+                                 " (" + Configuration.COL_PROFILE_ID + ", " + Configuration.COL_PROFILE_NAME +
+                                 " ," + Configuration.COL_METADATA_SOURCE_LOCATION + ", " + Configuration.COL_SYNC_SOURCE_ID +
                                  ") VALUES (@id, @name, @meta, @source)";
 
                 SqliteParameterCollection paramList = new SqliteParameterCollection();
@@ -223,11 +212,11 @@ namespace OneSync.Synchronization
 
         public override bool ProfileExists(string profileName)
         {
-            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, DATABASE_NAME)))
+            using (SQLiteAccess db = new SQLiteAccess(Path.Combine(this.StoragePath, Configuration.DATABASE_NAME)))
             {
                 // TODO: Change sql to SELECT COUNT(*)?
-                string cmdText = "SELECT * FROM " + PROFILE_TABLE + " WHERE "
-                                 + COL_PROFILE_NAME + " = @profileName";
+                string cmdText = "SELECT * FROM " + Configuration.TBL_PROFILE + " WHERE "
+                                 + Configuration.COL_PROFILE_NAME + " = @profileName";
 
                 SqliteParameterCollection paramList = new SqliteParameterCollection();
                 paramList.Add(new SqliteParameter("@profileName", System.Data.DbType.String) { Value = profileName });
