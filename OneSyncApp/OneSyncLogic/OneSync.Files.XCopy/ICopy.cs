9ceed37fd1b2c404959338f413094773e592981c
gclin009 <gclin009@3b441868-20d6-11df-a502-17d33bbe96c3>
2010-03-18 05:49:24 +0000

    Added items remotely
    C:\Users\u0707373\Desktop\[Team3][V0.9]\V0.9

---
+++ OneSyncApp/OneSyncLogic/OneSync.Files.XCopy/ICopy.cs
@@ -0,0 +1,15 @@
+﻿/*
+ $Id: ICopy.cs 66 2010-03-10 07:48:55Z gclin009 $
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneSync.Files.XCopy
+{
+    public interface ICopy
+    {
+        void Copy();       
+    }
+}
---
+++ OneSyncApp/OneSyncLogic/OneSync/Domain/Patch.cs
@@ -0,0 +1,70 @@
+﻿/*
+ $Id: Patch.cs 138 2010-03-16 11:28:14Z deskohet $
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Community.CsharpSqlite.SQLiteClient;
+using Community.CsharpSqlite;
+
+
+namespace OneSync.Synchronization
+{
+
+
+    public class Patch
+    {
+
+
+        /// <summary>
+        /// Creates a new Patch.
+        /// </summary>
+        /// <param name="syncSource">SyncSource of other PC where patch is to be applied.</param>
+        /// <param name="iStorage">Information about intermediary storage</param>
+        /// <param name="actions">Actions to be contained in this patch.</param>
+        public Patch(Profile profile)
+        {
+            //this.profile = profile;
+
+        }
+
+
+
+        /// <summary>
+        /// Verify whether all the files contained in this patch are valid by
+        /// checking whether it exists and optionally its file hash is the same.
+        /// </summary>
+        /// <param name="checkHash">Computes hash of files and verify it.</param>
+        /// <returns>True if all the files are valid.</returns>
+        /// 
+        /*
+        public bool Verify(bool checkHash)
+        {
+            // Root directory where all dirty files are stored
+            string rootDir = profile.IntermediaryStorage.DirtyFolderPath;
+            foreach (SyncAction a in actions)
+            {
+                // Skip Delete Action as there is no need to check for
+                // if file exists, neither is there a need to compute hash.
+                if (a is DeleteAction) continue;
+
+                string filePath = Path.Combine(rootDir, a.RelativeFilePath);
+
+                bool result = File.Exists(filePath);
+
+                // Check whether file hash tally
+                if (checkHash)
+                    result &= Files.FileUtils.GetFileHash(filePath).Equals(a.FileHash);
+
+                if (result == false) return false;
+            }
+            return true;
+        }*/
+
+
+
+
+    }
+}
