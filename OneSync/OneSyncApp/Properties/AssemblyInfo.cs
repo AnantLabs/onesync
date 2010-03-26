56e4a6f38deaccf072c8160aacf0975784f28a64
gclin009 <gclin009@3b441868-20d6-11df-a502-17d33bbe96c3>
2010-03-26 14:01:56 +0000

    Sign the assembly files with strong name.

--- OneSync/OneSyncApp/Properties/AssemblyInfo.cs
+++ OneSync/OneSyncApp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OneSyncWPF")]
+[assembly: AssemblyTitle("OneSync")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NUS")]
-[assembly: AssemblyProduct("OneSyncWPF")]
-[assembly: AssemblyCopyright("Copyright © NUS 2010")]
+[assembly: AssemblyCompany("CodeDroids")]
+[assembly: AssemblyProduct("OneSync")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
