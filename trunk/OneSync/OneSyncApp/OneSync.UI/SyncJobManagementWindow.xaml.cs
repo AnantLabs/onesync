9a357d9697869fe8efffcdf70055f90bd099f893
gclin009 <gclin009@3b441868-20d6-11df-a502-17d33bbe96c3>
2010-03-27 22:32:03 +0000

    After the bug bash, there are people complaining that our GUI is not user-friendly, so I have changed the previous GUI. The new GUI is very different from the previous one, so I call it as earthquake.

---
+++ trunk/OneSync/OneSyncApp/OneSync.UI/SyncJobManagementWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using OneSync.Synchronization;
+
+namespace OneSync.UI
+{
+	/// <summary>
+	/// Interaction logic for SyncJobManagementWindow.xaml
+	/// </summary>
+	public partial class SyncJobManagementWindow : Window
+	{
+        private SyncJob editingSyncJob;
+        private SyncJobManager profileManager;
+
+		public SyncJobManagementWindow()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+        public SyncJobManagementWindow(SyncJob selectedSyncJob, SyncJobManager inputProfileManager)
+            : this()
+		{
+            profileManager = inputProfileManager;
+            editingSyncJob = selectedSyncJob;
+            txtSyncJobName.Text = editingSyncJob.Name;
+            txtSource.Text = editingSyncJob.SyncSource.Path;
+            txtIntStorage.Text = editingSyncJob.IntermediaryStorage.Path;
+		}
+
+		private void btnBrowse_Source_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSource.Text = fbd.SelectedPath;
+                txtSource.Focus();
+            }
+		}
+
+		private void btnBrowse_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtIntStorage.Text = fbd.SelectedPath;
+                txtIntStorage.Focus();
+            }
+		}
+
+		private void txtBlkEditJob_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+            //Check the three required inputs for a new sync job:
+            // 1. Sync Job Name;
+            // 2. Sync Source Folder Directory;
+            // 3. Intermediate Storage Location.
+            string syncJobName = txtSyncJobName.Text.Trim();
+            string syncSourceDir = txtSource.Text.Trim();
+            string intStorageDir = txtIntStorage.Text.Trim();
+
+            string errorMsg = Validator.validateSyncJobName(syncJobName);
+            if (errorMsg != null)
+            {
+                showErrorMsg(errorMsg);
+                return;
+            }
+
+            errorMsg = Validator.validateSyncDirs(syncSourceDir, intStorageDir);
+            if (errorMsg != null)
+            {
+                showErrorMsg(errorMsg);
+                return;
+            }
+
+            //Update the job if all three inputs mentioned above are valid.
+            editingSyncJob.Name = syncJobName;
+            editingSyncJob.SyncSource.Path = syncSourceDir;
+            editingSyncJob.IntermediaryStorage.Path = intStorageDir;
+
+            if (!profileManager.Update(editingSyncJob))
+                showErrorMsg("Unable to update sync job at this moment.");
+
+            this.Close();
+		}
+
+		private void txtBlkDeleteJob_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+            DialogResult result = System.Windows.Forms.MessageBox.Show(
+                            "Are you sure you want to delete " + editingSyncJob.Name + "?", "Job Profile Deletion",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                if (!profileManager.Delete(editingSyncJob))
+                    showErrorMsg("Unable to delete sync job at this moment.");
+            }
+            this.Close();
+		}
+
+        private void showErrorMsg(string message)
+        {
+            if (String.IsNullOrEmpty(message))
+            {
+                label_notification.Visibility = Visibility.Hidden;
+                label_notification.Content = "";
+            }
+            else
+            {
+                label_notification.Content = message;
+                label_notification.Visibility = Visibility.Visible;
+            }
+        }
+	}
+}
---
+++ trunk/OneSync/OneSyncApp/OneSync.UI/UISyncJob.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace OneSync.UI
+{
+    class UISyncJob
+    {
+        private ObservableCollection<UISyncJobEntry> _SyncJobEntries = new ObservableCollection<UISyncJobEntry>();
+
+        public ObservableCollection<UISyncJobEntry> SyncJobEntries
+        {
+            get { return _SyncJobEntries; }
+        }
+
+        /// <summary>
+        /// ???
+        /// </summary>
+        /// <param name="fileName">The name of the processed file.</param>
+        /// <param name="status">The status of the process: 0 - Completely processed; 1 - Conflict.</param>
+        /// <param name="message">A detailed message.</param>
+        public void Add(string syncJobName, string syncSourceDir, string intStorageDir)
+        {
+            UISyncJobEntry entry = new UISyncJobEntry();
+
+            entry.JobName = syncJobName;
+            entry.SyncSource = syncSourceDir;
+            entry.IntStorage = intStorageDir;
+            entry.DeleteJob = "";
+
+            //Add a new sync job entry to the sync jobs collection.
+            _SyncJobEntries.Add(entry);
+        }
+    }
+
+    // Private class to represent a sync job entry
+    public class UISyncJobEntry
+    {
+        public string JobName { get; set; }
+        public string SyncSource { get; set; }
+        public string IntStorage { get; set; }
+        public string DeleteJob { get; set; }
+    }
+}
