3a0ca1fe5210456bcab9e1344e86d746b3bf77db
thuatvng <thuatvng@3b441868-20d6-11df-a502-17d33bbe96c3>
2010-04-09 09:54:29 +0000

    New FIles

---
+++ OneSync/OneSyncApp/OneSync.Logic/Domain/FolderMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OneSync.Synchronization
+{
+    public class FolderMetadata
+    {      
+        public IList<FolderMetadataItem> folderItems = new List<FolderMetadataItem>();
+        public FolderMetadata (string sourceId, string baseFolder)
+        {
+            SourceId = sourceId;
+            BaseFolder = baseFolder;
+        }
+        public string SourceId
+        { set;get; }
+
+        public string BaseFolder
+        { set;  get; }
+        public IList<FolderMetadataItem> FolderMetadataItems
+        {
+            set { folderItems = value; }
+            get { return this.folderItems; }
+        }
+    }
+}
